05510d7b05d14e4264729de463b9ff258ad884af
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2023-08-31 23:39:00 +0700

    nodejs to v18.17 and improve syntax highlight fish

--- fish/config.fish
+++ fish/config.fish
@@ -38,7 +38,7 @@ set -gx PATH $HOME/go/bin $PATH
 # source (semver completion fish | psub)
 
 # nodejs
-set -gx PATH "$HOME/.nvm/versions/node/v18.14.0/bin" $PATH
+set -gx PATH "$HOME/.nvm/versions/node/v18.17.0/bin" $PATH
 
 # pnpm
 set -gx PNPM_HOME "/Users/hammurabi/Library/pnpm"
@@ -59,3 +59,27 @@ alias ls="exa --icons -s type"
 alias du=dust
 alias cd=z
 alias lgit=lazygit
+
+# Fish syntax highlighting
+set -g fish_color_autosuggestion '555'  'brblack'
+set -g fish_color_cancel -r
+set -g fish_color_command --bold
+set -g fish_color_comment grey
+set -g fish_color_cwd green
+set -g fish_color_cwd_root red
+set -g fish_color_end brmagenta
+set -g fish_color_error brred
+set -g fish_color_escape 'bryellow' '--bold'
+set -g fish_color_history_current --bold
+set -g fish_color_host normal
+set -g fish_color_match --background=brblue
+set -g fish_color_normal normal
+set -g fish_color_operator bryellow
+set -g fish_color_param cyan
+set -g fish_color_quote yellow
+set -g fish_color_redirection brblue
+set -g fish_color_search_match 'bryellow'  '--background=brblack'
+set -g fish_color_selection 'white'  '--bold'  '--background=brblack'
+set -g fish_color_user brgreen
+set -g fish_color_valid_path --underline
+
